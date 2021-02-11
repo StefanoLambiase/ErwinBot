51bffa526164b5b18c79abd6489d1fe2f1af8323
ACupito <acupito2@gmail.com>
2021-02-11 17:10:36 +0100

    code clearing

--- dialogs/scrumDialogs/questionsDefinitionDialog.js
+++ dialogs/scrumDialogs/questionsDefinitionDialog.js
@@ -29,21 +29,6 @@ class QuestionsDefinitionDialog extends ComponentDialog {
         this.initialDialogId = WATERFALL_DIALOG;
     }
 
-    /**
-     * The run method handles the incoming activity (in the form of a TurnContext) and passes it through the dialog system.
-     * If no dialog is active, it will start the default dialog.
-     *
-    async run(turnContext, accessor) {
-        const dialogSet = new DialogSet(accessor);
-        dialogSet.add(this);
-
-        const dialogContext = await dialogSet.createContext(turnContext);
-        const results = await dialogContext.continueDialog();
-        if (results.status === DialogTurnStatus.empty) {
-            await dialogContext.beginDialog(this.id);
-        }
-    } */
-
     async definitionStep(step) {
         // check if the value inserted is an array and continue the process with it
         // if not instance a new one
